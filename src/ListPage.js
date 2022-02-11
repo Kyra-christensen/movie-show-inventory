954cbbb98be9d23232630fc669e48224d803d78a
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-11 02:22:28 -0800

    set state and created useEffect getAllMovSho function

--- src/ListPage.js
+++ src/ListPage.js
@@ -3,6 +3,16 @@ import { getAllMovSho } from './services/fetch-utils';
 import SingleMoSho from './SingleMoSho';
 
 export default function ListPage() {
+  const [movSho, setMovSho] = useState([]);
+
+  useEffect(() => {
+    async function fetch() {
+      const data = await getAllMovSho();
+      setMovSho(data);
+    }
+    fetch();
+  }, []);
+
   return (
     <div>ListPage</div>
   );
