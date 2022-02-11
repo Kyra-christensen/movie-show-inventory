4e689c2497575e0abf8e6caeb9e9aba9f58a9065
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-11 02:20:26 -0800

    imported necessary hooks, functions and files

---
+++ src/ListPage.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import { getAllMovSho } from './services/fetch-utils';
+import SingleMoSho from './SingleMoSho';
+
+export default function ListPage() {
+  return (
+    <div>ListPage</div>
+  );
+}
