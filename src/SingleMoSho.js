bab6f9c71fdfca6399be250dea42a1485c9a8952
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-11 02:47:23 -0800

    created a component that renders out a single movie show detail

--- src/SingleMoSho.js
+++ src/SingleMoSho.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Link from 'react-router-dom';
+
 export default function SingleMoSho({ movieShow }) {
   return (
-    
+    <Link to={`/movies-shows/${movieShow.id}`}>
+      <div className='single-movie-show'>
+        <h3>{movieShow.title}</h3>
+        <p>Directed by {movieShow.director}, is a {movieShow.type} released in {movieShow.year_released} that falls in the genre {movieShow.genre}.</p>
+      </div>
+    </Link>
   );
 }
